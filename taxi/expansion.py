94441c00c5a5c696f27e1302f28da775c3a5b64f
Igor Cegelnyk <il.cegelnik@gmail.com>
2022-08-12 11:18:37 +0300

    Refactored license_validation decorator

--- taxi/expansion.py
+++ taxi/expansion.py
@@ -6,11 +6,11 @@ def license_validation(func):
         license_number = func(args[0])
         letter = license_number[:3]
         number = license_number[3:]
-        if len(license_number) > 8:
+        if len(license_number) != 8:
             raise ValidationError("License number must contain exactly 8 characters")
         if not letter.isupper() or not letter.isalpha():
             raise ValidationError("First 3 characters are uppercase letters")
-        if not number.isdigit():
+        if not "".join(number).isdigit():
             raise ValidationError("Last 5 characters are digits")
         return license_number
     return inner
